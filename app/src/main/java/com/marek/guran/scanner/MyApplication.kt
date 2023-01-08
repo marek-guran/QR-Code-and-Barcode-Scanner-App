fc00f4037acaa6bb68a3cca4470b13f59d393e6d
Marek Guráň <skemgi2@gmail.com>
2023-01-08 14:47:35 +0100

    Small update

--- app/src/main/java/com/marek/guran/scanner/MyApplication.kt
+++ app/src/main/java/com/marek/guran/scanner/MyApplication.kt
@@ -1,12 +1,14 @@
 package com.marek.guran.scanner
 
+import android.annotation.SuppressLint
 import android.app.Application
 import com.google.android.material.color.DynamicColors
 
 class MyApplication : Application() {
 
+    @SuppressLint("MissingSuperCall")
     override fun onCreate() {
-        super.onCreate()
+        // Apply dynamic color
         DynamicColors.applyToActivitiesIfAvailable(this)
     }
 }
